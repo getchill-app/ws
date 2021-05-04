9e1738caa70da693a9c9e1e332864a034a7146c1
Gabriel Handford <gabrielh@gmail.com>
2021-05-03 18:02:58 -0700

    api: Event.Channel

--- api/events.go
+++ api/events.go
@@ -13,7 +13,13 @@ type Event struct {
 	Type  string `json:"type" msgpack:"type"`
 	Token string `json:"token,omitempty" msgpack:"token,omitempty"`
 
-	Vault *Vault `json:"vault,omitempty" msgpack:"vault,omitempty"`
+	Channel *Channel `json:"channel,omitempty" msgpack:"channel,omitempty"`
+	Vault   *Vault   `json:"vault,omitempty" msgpack:"vault,omitempty"`
+}
+
+type Channel struct {
+	KID   keys.ID `json:"kid" msgpack:"kid"`
+	Index int64   `json:"idx" msgpack:"idx"`
 }
 
 type Vault struct {
