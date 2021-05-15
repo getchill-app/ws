2e7dadf924029d8f0c777871b90a3f13b6c71f06
Gabriel Handford <gabrielh@gmail.com>
2021-05-15 13:26:14 -0700

    api: EventType

--- api/events.go
+++ api/events.go
@@ -7,22 +7,30 @@ import (
 // EventPubSub is the pub/sub name for events.
 const EventPubSub = "e"
 
+type EventType string
+
+const (
+	ChannelType  EventType = "channel"
+	VaultType    EventType = "vault"
+	ChannelsType EventType = "channels"
+)
+
 // Event to client.
 // JSON is used for websocket clients.
 type Event struct {
-	Type  string `json:"type" msgpack:"type"`
-	Token string `json:"token,omitempty" msgpack:"token,omitempty"`
+	Type  EventType `json:"type" msgpack:"type"`
+	Token string    `json:"token,omitempty" msgpack:"token,omitempty"`
 
 	Channel *Channel `json:"channel,omitempty" msgpack:"channel,omitempty"`
 	Vault   *Vault   `json:"vault,omitempty" msgpack:"vault,omitempty"`
 }
 
 type Channel struct {
-	KID   keys.ID `json:"kid" msgpack:"kid"`
+	ID    keys.ID `json:"id" msgpack:"id"`
 	Index int64   `json:"idx" msgpack:"idx"`
 }
 
 type Vault struct {
-	KID   keys.ID `json:"kid" msgpack:"kid"`
+	ID    keys.ID `json:"id" msgpack:"id"`
 	Index int64   `json:"idx" msgpack:"idx"`
 }
