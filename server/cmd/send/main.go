ee390a8ba05b33857892b33acd7abfb2fbf0bed9
Gabriel Handford <gabrielh@gmail.com>
2021-05-15 13:29:16 -0700

    server: Update go.mod

--- server/cmd/send/main.go
+++ server/cmd/send/main.go
@@ -9,21 +9,8 @@ import (
 	"github.com/getchill-app/ws/server"
 	"github.com/joho/godotenv"
 	"github.com/keys-pub/keys"
-	"github.com/keys-pub/keys/encoding"
-	"github.com/pkg/errors"
 	"github.com/vmihailenco/msgpack/v4"
 )
-
-func decodeKey(secretKey string) (*[32]byte, error) {
-	if secretKey == "" {
-		return nil, errors.Errorf("empty secret key")
-	}
-	key, err := encoding.Decode(secretKey, encoding.Hex)
-	if err != nil {
-		return nil, err
-	}
-	return keys.Bytes32(key), nil
-}
 
 func main() {
 	err := godotenv.Load()
@@ -47,9 +34,9 @@ func main() {
 	}
 
 	for i := 0; i < 20; i += 2 {
-		channel := keys.NewEdX25519KeyFromSeed(testSeed(byte(i)))
+		vault := keys.NewEdX25519KeyFromSeed(testSeed(byte(i)))
 		token := fmt.Sprintf("testtoken%d", i)
-		if err := send(&api.Event{Type: "vault", Vault: &api.Vault{KID: channel.ID(), Index: 1}, Token: token}); err != nil {
+		if err := send(&api.Event{Type: api.VaultType, Vault: &api.Vault{ID: vault.ID(), Index: 1}, Token: token}); err != nil {
 			log.Fatal(err)
 		}
 	}
--- server/go.mod
+++ server/go.mod
@@ -3,11 +3,10 @@ module github.com/getchill-app/ws/server
 go 1.16
 
 require (
-	github.com/getchill-app/ws/api v0.0.0-20210426215843-9687d38a8bda
+	github.com/getchill-app/ws/api v0.0.0-20210515202614-2e7dadf92402
 	github.com/gomodule/redigo v1.8.4
 	github.com/gorilla/websocket v1.4.2
 	github.com/joho/godotenv v1.3.0
 	github.com/keys-pub/keys v0.1.21
-	github.com/pkg/errors v0.9.1
 	github.com/vmihailenco/msgpack/v4 v4.3.12
 )
